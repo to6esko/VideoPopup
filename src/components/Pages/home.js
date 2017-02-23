3cad43a2e4b95794d7649e448a5e4136c4b3bdea
Todor Krastev <to6esko69@gmail.com>
2017-02-23 19:02:27 +0200

    add style.css

--- src/components/Pages/home.js
+++ src/components/Pages/home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-export default class Home extends React.Component{
+export default class Home extends React.Component {
     render() {
         return (
-            <div>
-                <h1>Hello React</h1>
+            <div className="back-ground">
+                <div className="dot">
+                    <img src="img/got.jpg" alt="dot" />
                 </div>
+            </div>
         )
     }
 }
