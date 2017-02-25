90d2547378976b8603267a468146f7df081e8050
Todor Krastev <to6esko69@gmail.com>
2017-02-25 05:30:54 +0200

    add Reply...

--- src/components/Pages/commetBox.js
+++ src/components/Pages/commetBox.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-export default class CommentBox extends React.Component{
+export default class CommentBox extends React.Component {
     render() {
         return (
             <div className="border-bottom">
-            <div className="commentBox">
-                    <input type="text" className="comment" name="comment" placeholder="comment.."/>
-            </div>
+                <div className="commentBox">
+                    <input type="text" className="comment" name="comment" placeholder="comment..." />
                 </div>
+            </div>
         )
     }
 }
--- src/components/Pages/footerButtons.js
+++ src/components/Pages/footerButtons.js
@@ -4,13 +4,23 @@ export default class FooterButtons extends React.Component {
     render() {
         return (
             <div className="btn-footer">
-                    <ul>
-                        <li><img src="img/btn-like.jpg" alt="like" /></li>
-                        <li><img src="img/btn-share.jpg" alt="share" /></li>
-                        <li><img src="img/btn-coment.jpg" alt="share" /></li>
-                        <li className="btn-footer-right"><img src="img/btn-report.jpg" alt="share" /></li>
-                    </ul>
+                <ul>
+                    <li><img src="img/btn-like.jpg" alt="like" /></li>
+                    <li><img src="img/btn-share.jpg" alt="share" /></li>
+                    <li><img src="img/btn-coment.jpg" alt="share" /></li>
+                    <li className="btn-footer-right"><img src="img/btn-report.jpg" alt="share" /></li>
+                </ul>
                 <div className="footerBottom">
+                </div>
+                <div className="footer-btn">
+                    <ul>
+                        <li><img src="img/footer-comment.jpg" alt="like" /></li>
+                        <li><img src="img/btn-photo.jpg" alt="share" /></li>
+                        <li><img src="img/btn-feedback.jpg" alt="share" /></li>
+                    </ul>
+                </div>
+                <div className="reply">
+                    <input type="text" className="reply-comment" name="reply" placeholder="Reply..." />
                 </div>
             </div>
         )
